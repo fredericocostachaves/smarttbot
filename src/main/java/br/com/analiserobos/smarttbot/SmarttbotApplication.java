3bc9bbab8915e343993594422ebf3a545653cc0d
Frederico Costa Chaves <frederico.chaves@mj.gov.br>
2022-07-06 16:56:25 -0300

    otimozação código

--- src/main/java/br/com/analiserobos/smarttbot/SmarttbotApplication.java
+++ src/main/java/br/com/analiserobos/smarttbot/SmarttbotApplication.java
@@ -1,6 +1,7 @@
 package br.com.analiserobos.smarttbot;
 
 import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -21,46 +22,19 @@ public class SmarttbotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmarttbotApplication.class, args);
-		try (CSVReader reader = new CSVReader(new FileReader("C:\\Users\\fcost\\git\\smarttbot\\src\\main\\resources\\resultados\\Johannes_Brahms.csv"))) {			
-			String[] nextLine;
-			boolean primeiraLinha = true;
-			List<DiaOperacao> diasOperacao = new ArrayList<>();
+		try (CSVReader reader = new CSVReader(new FileReader("C:\\Users\\fcost\\git\\smarttbot\\src\\main\\resources\\resultados\\Sao_Lourenco.csv"))) {						
 			logger.info("Iniciando Análise");
-			//CSV: 6821290961";"17/09/2018 / 10:08:32";"WINV18";"V";"2";"75.575,00";"executada";"entrada";"2";"75.580,00";"-";"-";"-
-			while ((nextLine = reader.readNext()) != null) {
-				for (String token : nextLine) {
-					if (primeiraLinha) {
-						primeiraLinha = false;
-					} else {
-						DiaOperacao diaOperacao = null;
-						String[] linha = token.split(";");
-						String linhaSTR = linha[1].substring(7,11) + '-' + linha[1].substring(4,6) + '-' + linha[1].substring(1, 3);						
-						LocalDate diaLinha = LocalDate.parse(linhaSTR);						
-						Optional<DiaOperacao> diaOptional = diasOperacao.stream().filter(o -> o.getData().equals(diaLinha)).findFirst();
-						if (diaOptional.isEmpty()) {
-							diaOperacao = new DiaOperacao();
-							diaOperacao.setData(diaLinha);
-							diasOperacao.add(diaOperacao);
-						} else {
-							diaOperacao = diaOptional.get();
-						}						
-						if (!linha[12].equals("\"-")) {
-							String resultadoSTR = linha[12].replace(".", "");
-							resultadoSTR = resultadoSTR.replace(",", ".").replace("\"", ""); 
-							Double resultadoLinha = Double.parseDouble(resultadoSTR);
-							diaOperacao.setResultado(diaOperacao.getResultado()+resultadoLinha);
-						}
-					}
-				}				
-			}
+			List<DiaOperacao> diasOperacao = new ArrayList<>();
+			diasOperacao = processaLinhas(reader);
 			int diasGainsConsecutivos = 0;
 			int diasLossConsecutivos = 0;
 			Double mediaGainsConsecutivos = 0.0;
 			Double mediaLossConsecutivos = 0.0;
 			DiaOperacao diaAnterior = new DiaOperacao();
 			Collections.reverse(diasOperacao);
-			for(DiaOperacao dia: diasOperacao) {				
-				logger.info(dia.toString());
+			for(DiaOperacao dia: diasOperacao) {
+				String diaSTR = dia.toString();
+				logger.info(diaSTR);
 				if (dia.getResultado() > 0) {
 					if (diaAnterior.getResultado() < 0) {
 						mediaGainsConsecutivos = (mediaGainsConsecutivos + diasGainsConsecutivos)/2;
@@ -76,13 +50,47 @@ public class SmarttbotApplication {
 				}
 				diaAnterior = dia;
 			}
-			logger.info("Média de gains consecutivos: "+mediaGainsConsecutivos);
-			logger.info("Média de loss consecutivos: "+mediaLossConsecutivos);			
+			String mediaGainsSTR = "Média de gains consecutivos: "+mediaGainsConsecutivos;
+			String mediaLossSTR = "Média de loss consecutivos: "+mediaLossConsecutivos;
+			logger.info(mediaGainsSTR);
+			logger.info(mediaLossSTR);			
 			logger.info("Fim Análise");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	
+	private static List<DiaOperacao> processaLinhas(CSVReader reader) throws NumberFormatException, IOException {
+		//CSV: 6821290961";"17/09/2018 / 10:08:32";"WINV18";"V";"2";"75.575,00";"executada";"entrada";"2";"75.580,00";"-";"-";"-
+		String[] nextLine;
+		boolean primeiraLinha = true;
+		List<DiaOperacao> diasOperacao = new ArrayList<>();
+		while ((nextLine = reader.readNext()) != null) {
+			for (String token : nextLine) {
+				if (primeiraLinha) {
+					primeiraLinha = false;
+				} else {
+					DiaOperacao diaOperacao = null;
+					String[] linha = token.split(";");
+					String linhaSTR = linha[1].substring(7,11) + '-' + linha[1].substring(4,6) + '-' + linha[1].substring(1, 3);						
+					LocalDate diaLinha = LocalDate.parse(linhaSTR);						
+					Optional<DiaOperacao> diaOptional = diasOperacao.stream().filter(o -> o.getData().equals(diaLinha)).findFirst();
+					if (diaOptional.isEmpty()) {
+						diaOperacao = new DiaOperacao();
+						diaOperacao.setData(diaLinha);
+						diasOperacao.add(diaOperacao);
+					} else {
+						diaOperacao = diaOptional.get();
+					}						
+					if (!linha[12].equals("\"-")) {
+						String resultadoSTR = linha[12].replace(".", "");
+						resultadoSTR = resultadoSTR.replace(",", ".").replace("\"", ""); 
+						Double resultadoLinha = Double.parseDouble(resultadoSTR);
+						diaOperacao.setResultado(diaOperacao.getResultado()+resultadoLinha);
+					}
+				}
+			}				
+		}
+		return diasOperacao;
+	}
 }
